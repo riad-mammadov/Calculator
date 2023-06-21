cbc65ebea30e556f9a858a7ac6a9cc95b0291470
riadmammadov <riad.mammadov@city.ac.uk>
2023-06-21 17:46:43 +0100

    Add project files.

---
+++ Calculator/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int num1, num2;
+	char op;
+
+	cout << "Enter two numbers" << '\n';
+	cin >> num1 >> num2;
+
+	cout << "Now enter an operation (+,-,*,/,%)" << '\n';
+	cin >> op;
+
+
+
+
+
+
+
+
+}
